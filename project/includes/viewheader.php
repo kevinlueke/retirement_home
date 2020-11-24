61a021620e8b151130e0964d4fb2495bf6464f24
Scott Johnson <scottyjohnson002@gmail.com>
2020-11-24 10:12:46 -0500

    change the format of the view directory

--- project/includes/viewheader.php
+++ project/includes/viewheader.php
@@ -10,16 +10,26 @@ require_once 'register-inc.php';
   <head>
     <meta charset="utf-8">
     <title>Shady Acres</title>
-    <link rel="stylesheet" type="text/css" href="../style.css">
+    <link rel="stylesheet" type="text/css" href="../../style.css">
   </head>
   <body>
   <header>
+
     <nav>
       <ul>
-        <li> <a href="../logout.php">Log Out</a></li>
+
         <?php
           //conditionals for future navigation depending on $_SESSION['accessLevel']
+          if ($_SESSION['accessLevel']==0){ //admin
+          echo ' <li> <a href="../admin/adminhome.php">Home</a></li>';
+          echo ' <li> <a href="../admin/employees.php">Employees</a></li>';
+          echo ' <li> <a href="../admin/approve.php">Approve</a></li>';
+          echo ' <li> <a href="../admin/Roles.php">Roles</a></li>';
+
+          }
          ?>
+
+         <li> <a href="../logout.php">Log Out</a></li>
       </ul>
     </nav>
   </header>
